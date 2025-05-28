6e6c7b0b846d26de322be7a0e39ae1d2dfc7ee1b
Atanas Dinov <atanas.dinov@suse.com>
2025-05-28 17:57:54 +0300

    kk
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- internal/cli/elemental-toolkit/action/install.go
+++ internal/cli/elemental-toolkit/action/install.go
@@ -44,7 +44,7 @@ func Install(ctx *cli.Context) error { //nolint:dupl
 
 	s.Logger().Info("Starting install action with args: %+v", args)
 
-	d, err := digestInstallSetup(s, args)
+	d, err := DigestInstallSetup(s, args)
 	if err != nil {
 		s.Logger().Error("failed to collect installation setup: %v", err)
 		return err
@@ -80,7 +80,7 @@ func Install(ctx *cli.Context) error { //nolint:dupl
 	return nil
 }
 
-func digestInstallSetup(s *sys.System, flags *cmd.InstallFlags) (*deployment.Deployment, error) {
+func DigestInstallSetup(s *sys.System, flags *cmd.InstallFlags) (*deployment.Deployment, error) {
 	d := deployment.DefaultDeployment()
 	if flags.Description != "" {
 		if ok, _ := vfs.Exists(s.FS(), flags.Description); !ok {
