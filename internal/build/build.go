e76e025b3b1a8c6dc7e733dbe5241d2b7a9f6b50
Atanas Dinov <atanas.dinov@suse.com>
2025-05-29 14:03:45 +0300

    kkk
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- internal/build/build.go
+++ internal/build/build.go
@@ -148,7 +148,7 @@ func Run(ctx context.Context, d *image.Definition, buildDir string, l log.Logger
 		ConfigScript:         scriptPath,
 		Target:               device,
 		Overlay:              fmt.Sprintf("%s://%s", deployment.Tar, overlaysTar),
-		Bootloader:           "none",
+		Bootloader:           bootloader.BootGrub,
 	}
 
 	l.Info("Preparing installation setup")
