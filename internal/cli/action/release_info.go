48f848e75723e92b6c930b686563e0d7d4d5c471
Dharmit Shah <dharmit.shah@suse.com>
2026-03-18 14:17:34 +0530

    Changes to release_info.go using AI
    Base for the release-info flag was set in previous commit, albeit the
    code was unorganized. Changes in this commit were generated by AI and
    a few errors it made were corrected by me.

--- internal/cli/action/release_info.go
+++ internal/cli/action/release_info.go
@@ -28,6 +28,9 @@ import (
 	"text/tabwriter"
 
 	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/suse/elemental/v3/pkg/manifest/api"
+	"go.yaml.in/yaml/v3"
+
 	cmdpkg "github.com/suse/elemental/v3/internal/cli/cmd"
 	"github.com/suse/elemental/v3/internal/config"
 	"github.com/suse/elemental/v3/pkg/extractor"
@@ -40,21 +43,25 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
-// coreManifest struct here is only for the sake of `elemental3 release-info` command;
-// original struct is in pkg/manifest/api/core/manifest.go
-type coreManifest struct {
-	OperatingSystem   string   `yaml:"operatingSystem" json:"operatingSystem,omitempty"`
-	HelmCharts        []string `yaml:"helmCharts,omitempty" json:"helmCharts,omitempty"`
-	HelmRepos         []string `yaml:"helmRepos,omitempty" json:"helmRepos,omitempty"`
-	SystemdExtensions []string `yaml:"systemdExtensions,omitempty" json:"systemdExtensions,omitempty"`
+// ManifestInfo holds the structured information for display
+type ManifestInfo struct {
+	Core    *CoreInfo    `json:"core,omitempty" yaml:"core,omitempty"`
+	Product *ProductInfo `json:"product,omitempty" yaml:"product,omitempty"`
 }
 
-// productManifest struct here is only for the sake of `elemental3 release-info` command;
-// original struct is in pkg/manifest/api/product/manifest.go
-type productManifest struct {
-	SystemdExtensions []string `yaml:"systemdExtensions"`
-	HelmCharts        []string `yaml:"helmCharts"`
-	HelmRepos         []string `yaml:"helmRepos"`
+// CoreInfo represents core manifest details
+type CoreInfo struct {
+	OperatingSystem   string   `json:"operatingSystem" yaml:"operatingSystem"`
+	HelmCharts        []string `json:"helmCharts,omitempty" yaml:"helmCharts,omitempty"`
+	HelmRepos         []string `json:"helmRepos,omitempty" yaml:"helmRepos,omitempty"`
+	SystemdExtensions []string `json:"systemdExtensions,omitempty" yaml:"systemdExtensions,omitempty"`
+}
+
+// ProductInfo represents product manifest details
+type ProductInfo struct {
+	SystemdExtensions []string `json:"systemdExtensions,omitempty" yaml:"systemdExtensions,omitempty"`
+	HelmCharts        []string `json:"helmCharts,omitempty" yaml:"helmCharts,omitempty"`
+	HelmRepos         []string `json:"helmRepos,omitempty" yaml:"helmRepos,omitempty"`
 }
 
 func ReleaseInfo(ctx context.Context, cmd *cli.Command) error {
@@ -66,11 +73,21 @@ func ReleaseInfo(ctx context.Context, cmd *cli.Command) error {
 
 	system.Logger().Debug("release-info called with args: %+v", args)
 
-	// check if the provided arg is a local file or an OCI image
 	arg := cmd.Args().Get(0)
-	srcType, err := argSourceType(system, arg)
+	resolved, err := resolveManifest(ctx, system, arg, args.Local)
 	if err != nil {
 		return err
+	}
+
+	info := buildManifestInfo(resolved, args.Core, args.Product)
+
+	return printManifestInfo(info, args.Output)
+}
+
+func resolveManifest(ctx context.Context, system *sys.System, arg string, local bool) (*resolver.ResolvedManifest, error) {
+	srcType, err := argSourceType(system, arg)
+	if err != nil {
+		return nil, err
 	}
 	system.Logger().Debug("found source type: %s", srcType)
 
@@ -80,63 +97,141 @@ func ReleaseInfo(ctx context.Context, cmd *cli.Command) error {
 	}
 
 	if srcType == source.OCI {
-		_, err = name.ParseReference(uri)
-		if err != nil {
-			return fmt.Errorf("invalid OCI image reference: %w", err)
+		if _, err := name.ParseReference(uri); err != nil {
+			return nil, fmt.Errorf("invalid OCI image reference: %w", err)
 		}
 	}
 
 	output, err := config.NewOutput(system.FS(), "", "")
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer output.Cleanup(system.FS())
 
-	resolver, err := manifestResolver(system.FS(), output, args.Local)
+	res, err := manifestResolver(system.FS(), output, local)
 	if err != nil {
-		return err
-	}
-	resolvedManifest, err := resolver.Resolve(uri)
-	if err != nil {
-		return err
+		return nil, err
 	}
 
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
+	return res.Resolve(uri)
+}
 
-	coreManifestOut := generateCoreManifest(resolvedManifest.CorePlatform)
-	//var pmOut *productManifest
-	//if resolvedManifest.ProductExtension != nil {
-	//	pmOut = printProductManifest(w, resolvedManifest.ProductExtension)
-	//
-	//	fmt.Fprintf(w, "Product Manifest\n%s\n", strings.Repeat("-", len("Product Manifest")))
-	//	fmt.Fprintf(w, "Helm Charts\t %s\n", strings.Join(pmOut.HelmCharts, ", "))
-	//	fmt.Fprintf(w, "Helm Repositories\t %s \n", strings.Join(pmOut.HelmRepos, ", "))
-	//	fmt.Fprintln(w)
-	//}
-	//
-	//fmt.Fprintf(w, "Core Manifest\n%s\n", strings.Repeat("-", len("Core Manifest")))
-	//fmt.Fprintf(w, "Operating System\t %s\n", coreManifestOut.OperatingSystem)
-	//fmt.Fprintf(w, "Helm Charts\t %s\n", strings.Join(coreManifestOut.HelmCharts, ", "))
-	//fmt.Fprintf(w, "Helm Repositories\t %s \n", strings.Join(coreManifestOut.HelmRepos, ", "))
-	//
+func buildManifestInfo(resolved *resolver.ResolvedManifest, showCore, showProduct bool) *ManifestInfo {
+	info := &ManifestInfo{}
+
+	// If neither core nor product is specified, or both are specified, show both
+	defaultShow := (!showCore && !showProduct) || (showCore && showProduct)
+
+	if (showCore || defaultShow) && resolved.CorePlatform != nil {
+		info.Core = mapCoreInfo(resolved.CorePlatform)
+	}
+
+	if (showProduct || defaultShow) && resolved.ProductExtension != nil {
+		info.Product = mapProductInfo(resolved.ProductExtension)
+	}
+
+	return info
+}
+
+func printManifestInfo(info *ManifestInfo, format string) error {
+	switch strings.ToLower(format) {
+	case "json":
+		encoder := json.NewEncoder(os.Stdout)
+		encoder.SetIndent("", "  ")
+		return encoder.Encode(info)
+	case "yaml":
+		encoder := yaml.NewEncoder(os.Stdout)
+		return encoder.Encode(info)
+	case "":
+		printTable(info)
+		return nil
+	default:
+		return fmt.Errorf("unsupported output format: %s", format)
+	}
+}
+
+func printTable(info *ManifestInfo) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+
+	if info.Product != nil {
+		fmt.Fprintf(w, "Product Manifest\n%s\n", strings.Repeat("-", 16))
+		if len(info.Product.SystemdExtensions) > 0 {
+			fmt.Fprintf(w, "Systemd Extensions\t%s\n", strings.Join(info.Product.SystemdExtensions, ", "))
+		}
+		if len(info.Product.HelmCharts) > 0 {
+			fmt.Fprintf(w, "Helm Charts\t%s\n", strings.Join(info.Product.HelmCharts, ", "))
+		}
+		if len(info.Product.HelmRepos) > 0 {
+			fmt.Fprintf(w, "Helm Repositories\t%s\n", strings.Join(info.Product.HelmRepos, ", "))
+		}
+		fmt.Fprintln(w)
+	}
+
+	if info.Core != nil {
+		fmt.Fprintf(w, "Core Manifest\n%s\n", strings.Repeat("-", 13))
+		fmt.Fprintf(w, "Operating System\t%s\n", info.Core.OperatingSystem)
+		if len(info.Core.SystemdExtensions) > 0 {
+			fmt.Fprintf(w, "Systemd Extensions\t%s\n", strings.Join(info.Core.SystemdExtensions, ", "))
+		}
+		if len(info.Core.HelmCharts) > 0 {
+			fmt.Fprintf(w, "Helm Charts\t%s\n", strings.Join(info.Core.HelmCharts, ", "))
+		}
+		if len(info.Core.HelmRepos) > 0 {
+			fmt.Fprintf(w, "Helm Repositories\t%s\n", strings.Join(info.Core.HelmRepos, ", "))
+		}
+		fmt.Fprintln(w)
+	}
+
 	w.Flush()
-	//
-	//if pmOut != nil {
-	//	data, err := yaml.Marshal(&pmOut)
-	//	if err != nil {
-	//		return err
-	//	}
-	//	fmt.Println()
-	//	fmt.Println(string(data))
-	//}
-	data, err := json.Marshal(&coreManifestOut)
-	if err != nil {
-		return err
-	}
-	fmt.Println()
-	fmt.Println(string(data))
+}
 
-	return nil
+func mapCoreInfo(cm *core.ReleaseManifest) *CoreInfo {
+	osName := "Unknown"
+	if cm.Components.OperatingSystem.Image.Base != "" {
+		parts := strings.Split(cm.Components.OperatingSystem.Image.Base, ":")
+		if len(parts) > 1 {
+			osName = "SLES " + strings.Split(parts[1], "-")[0]
+		}
+	}
+
+	return &CoreInfo{
+		OperatingSystem:   osName,
+		HelmCharts:        mapHelmCharts(cm.Components.Helm.Charts),
+		HelmRepos:         mapHelmRepos(cm.Components.Helm.Repositories),
+		SystemdExtensions: mapSystemdExtensions(cm.Components.Systemd.Extensions),
+	}
+}
+
+func mapProductInfo(pm *product.ReleaseManifest) *ProductInfo {
+	return &ProductInfo{
+		SystemdExtensions: mapSystemdExtensions(pm.Components.Systemd.Extensions),
+		HelmCharts:        mapHelmCharts(pm.Components.Helm.Charts),
+		HelmRepos:         mapHelmRepos(pm.Components.Helm.Repositories),
+	}
+}
+
+func mapHelmCharts(charts []*api.HelmChart) []string {
+	var result []string
+	for _, h := range charts {
+		result = append(result, fmt.Sprintf("%s (%s)", h.GetName(), h.GetRepositoryName()))
+	}
+	return result
+}
+
+func mapHelmRepos(repos []*api.HelmRepository) []string {
+	var result []string
+	for _, r := range repos {
+		result = append(result, r.Name)
+	}
+	return result
+}
+
+func mapSystemdExtensions(exts []api.SystemdExtension) []string {
+	var result []string
+	for _, e := range exts {
+		result = append(result, e.Name)
+	}
+	return result
 }
 
 // argSourceType takes a string argument and returns if the release manifest source type is a file or an OCI image
@@ -159,51 +254,6 @@ func argSourceType(s *sys.System, arg string) (source.ReleaseManifestSourceType,
 		return source.File, nil
 	}
 	return source.OCI, nil
-}
-
-func generateCoreManifest(cm *core.ReleaseManifest) *coreManifest {
-	var os string
-	os = strings.Split(cm.Components.OperatingSystem.Image.Base, ":")[1]
-	os = "SLES " + strings.Split(os, "-")[0]
-
-	var helmCharts, helmRepos, systemdExtensions []string
-
-	for _, h := range cm.Components.Helm.Charts {
-		helmCharts = append(helmCharts, fmt.Sprintf("%s (%s)", h.GetName(), h.GetRepositoryName()))
-	}
-	for _, r := range cm.Components.Helm.Repositories {
-		helmRepos = append(helmRepos, r.Name)
-	}
-	for _, e := range cm.Components.Systemd.Extensions {
-		systemdExtensions = append(systemdExtensions, e.Name)
-	}
-
-	return &coreManifest{
-		OperatingSystem:   os,
-		HelmCharts:        helmCharts,
-		HelmRepos:         helmRepos,
-		SystemdExtensions: systemdExtensions,
-	}
-}
-
-func printProductManifest(w *tabwriter.Writer, pm *product.ReleaseManifest) *productManifest {
-	var systemdExtensions, helmCharts, helmRepos []string
-
-	for _, e := range pm.Components.Systemd.Extensions {
-		systemdExtensions = append(systemdExtensions, e.Name)
-	}
-	for _, h := range pm.Components.Helm.Charts {
-		helmCharts = append(helmCharts, fmt.Sprintf("%s (%s)", h.GetName(), h.GetRepositoryName()))
-	}
-	for _, r := range pm.Components.Helm.Repositories {
-		helmRepos = append(helmRepos, r.Name)
-	}
-
-	return &productManifest{
-		SystemdExtensions: systemdExtensions,
-		HelmCharts:        helmCharts,
-		HelmRepos:         helmRepos,
-	}
 }
 
 func manifestResolver(fs vfs.FS, out config.Output, local bool) (*resolver.Resolver, error) {
